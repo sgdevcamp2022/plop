2411387804df82038b4432f5d6c5ea0b791126e9
Joons <elddy0948@gmail.com>
2023-01-30 13:08:54 +0900

    chore : project init

---
+++ src/mobile/ios/plop-messenger-ios/plop-messenger-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  plop-messenger-ios
+//
+//  Created by 김호준 on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
