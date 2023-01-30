ebeb6e2d42e0f32fbd13be5ef2da232aecd0e615
Joons <elddy0948@gmail.com>
2023-01-30 13:21:09 +0900

    style: remove comments

--- src/mobile/ios/plop-messenger-ios/plop-messenger-ios/ViewController.swift
+++ src/mobile/ios/plop-messenger-ios/plop-messenger-ios/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  plop-messenger-ios
-//
-//  Created by 김호준 on 2023/01/30.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 
