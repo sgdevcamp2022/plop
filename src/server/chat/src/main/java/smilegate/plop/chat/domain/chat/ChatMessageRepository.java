67f7ecd6731b916d0f925f8eca8d92546015ea60
llsrrll96 <llsrrll96@gmail.com>
2023-01-31 19:30:15 +0900

    chore: Delete import

--- src/server/chat/src/main/java/smilegate/plop/chat/domain/chat/ChatMessageRepository.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/domain/chat/ChatMessageRepository.java
@@ -1,8 +1,6 @@
 package smilegate.plop.chat.domain.chat;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import smilegate.plop.chat.domain.room.RoomCollection;
-import smilegate.plop.chat.domain.room.RoomMongoTemplateRepository;
 
 public interface ChatMessageRepository extends MongoRepository<MessageCollection,String>, ChatMongoTemplateRepository {
 }
