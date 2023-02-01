2e828e6c625c3bdae0585407d16052710333edbf
llsrrll96 <llsrrll96@gmail.com>
2023-02-01 10:30:48 +0900

    feat: Add getAllLastMessage Method

--- src/server/chat/src/main/java/smilegate/plop/chat/domain/chat/ChatMessageRepositoryImpl.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/domain/chat/ChatMessageRepositoryImpl.java
@@ -1,4 +1,23 @@
 package smilegate.plop.chat.domain.chat;
 
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
 public class ChatMessageRepositoryImpl implements ChatMongoTemplateRepository{
+
+    private final MongoTemplate mongoTemplate;
+
+    public ChatMessageRepositoryImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public List<MessageCollection> getAllLastMessage(List<String> roomIds) {
+        Query query = Query.query(Criteria.where("roomId").in(roomIds));
+
+        return null;
+    }
 }
