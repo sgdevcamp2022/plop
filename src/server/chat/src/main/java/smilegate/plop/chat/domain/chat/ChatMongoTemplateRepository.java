d4881f82ef59679f9e76fe93bb78966106f8c6f4
llsrrll96 <llsrrll96@gmail.com>
2023-02-01 10:30:16 +0900

    feat: Add getAllLastMessage Method

--- src/server/chat/src/main/java/smilegate/plop/chat/domain/chat/ChatMongoTemplateRepository.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/domain/chat/ChatMongoTemplateRepository.java
@@ -1,4 +1,8 @@
 package smilegate.plop.chat.domain.chat;
 
+import java.util.List;
+
 public interface ChatMongoTemplateRepository {
+    List<MessageCollection> getAllLastMessage(List<String> roomIds);
+
 }
