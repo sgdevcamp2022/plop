f6735ab7436a97941ddf64cc9669a327118e88a4
llsrrll96 <llsrrll96@gmail.com>
2023-02-03 10:57:31 +0900

    feat: Add method from ChatMessageService
    getNewMessages

--- src/server/chat/src/main/java/smilegate/plop/chat/service/ChatMessageService.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/service/ChatMessageService.java
@@ -6,6 +6,8 @@ import smilegate.plop.chat.domain.chat.MessageCollection;
 import smilegate.plop.chat.dto.ChatMessageDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ChatMessageService {
@@ -22,10 +24,15 @@ public class ChatMessageService {
                 .roomId(chatMessageDto.getRoom_id())
                 .senderId(chatMessageDto.getSender_id())
                 .content(chatMessageDto.getContent())
-                .createAt(LocalDateTime.now())
+                .createdAt(LocalDateTime.now())
                 .build());
 
         return convertEntityToDto(messageCollection);
+    }
+
+    public List<ChatMessageDto> getNewMessages(String roomId, String readMsgId){
+        List<MessageCollection> messageCollections = chatMessageRepository.getNewMessages(roomId, readMsgId);
+        return messageCollections.stream().map(m -> convertEntityToDto(m)).collect(Collectors.toList());
     }
 
     private ChatMessageDto convertEntityToDto(MessageCollection messageCollection) {
@@ -34,7 +41,7 @@ public class ChatMessageService {
                 .message_type(messageCollection.getType())
                 .sender_id(messageCollection.getSenderId())
                 .content(messageCollection.getContent())
-                .created_at(messageCollection.getCreateAt())
+                .created_at(messageCollection.getCreatedAt())
                 .message_id(messageCollection.get_id())
                 .build();
     }
