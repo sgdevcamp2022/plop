aa4dc1aeff4d171ac5c4a0d1d34988d07d827de9
llsrrll96 <llsrrll96@gmail.com>
2023-02-03 10:55:31 +0900

    chore: use lombok annotation
    @ToString

--- src/server/chat/src/main/java/smilegate/plop/chat/dto/ChatMessageDto.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/dto/ChatMessageDto.java
@@ -1,39 +1,28 @@
 package smilegate.plop.chat.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class ChatMessageDto {
-
     private MessageType message_type;
     private String room_id;
     private String sender_id;
     private String content;
-    private String message_to;
     private String message_id;
     private LocalDateTime created_at;
 
     @Builder
-    public ChatMessageDto(String message_id, MessageType message_type, String room_id, String sender_id, String content, String message_to,LocalDateTime created_at) {
+    public ChatMessageDto(String message_id, MessageType message_type, String room_id, String sender_id, String content,LocalDateTime created_at) {
         this.message_id = message_id;
         this.message_type = message_type;
         this.room_id = room_id;
         this.sender_id = sender_id;
         this.content = content;
-        this.message_to = message_to;
         this.created_at = created_at;
-    }
-
-    @Override
-    public String toString() {
-        return "M_id: "+message_id+", room: "+room_id+ ", Type: "+message_type+"\n"+
-        ", sender: " + sender_id+", Content: "+content;
     }
 }
