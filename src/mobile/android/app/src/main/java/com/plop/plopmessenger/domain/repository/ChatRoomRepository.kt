f4579a16e6a55532dca5a6b29dd8c6d0e8d4ab39
GaheeKim <aqswefr@naver.com>
2023-02-03 17:17:15 +0900

    feat: add chat room method
    [Feat]: add chat room method
    
    1. 채팅룸 넣기 1 or All
    2. 채팅룸 수정
        - [x]  Update title by id
        - [x]  Update unread by id
        - [x]  plus unread by id
        - [x]  Update content and (update at) by id
    3. 채팅룸 삭제

--- src/mobile/android/app/src/main/java/com/plop/plopmessenger/domain/repository/ChatRoomRepository.kt
+++ src/mobile/android/app/src/main/java/com/plop/plopmessenger/domain/repository/ChatRoomRepository.kt
@@ -1,11 +1,22 @@
 package com.plop.plopmessenger.domain.repository
 
 import com.plop.plopmessenger.data.local.dao.ChatRoomMemberImage
+import com.plop.plopmessenger.data.local.entity.ChatRoom
 import kotlinx.coroutines.flow.Flow
+import java.util.*
 
 interface ChatRoomRepository {
     fun loadChatRoomTitle(chatroomId: String): Flow<String>
     fun loadChatRoomAndMessage(): Flow<List<ChatRoomMemberImage>>
     fun loadChatRoomAndMemberById(chatroomId: String): Flow<ChatRoomMemberImage>
     fun hasPersonalChatRoomByFriend(friendId: String): Flow<String?>
+    fun insertChatRoom(chatRoom: ChatRoom)
+    fun insertAllChatRoom(chatRooms: List<ChatRoom>)
+    fun updateChatRoom(chatroom: ChatRoom)
+    fun updateChatroomAll(chatrooms: List<ChatRoom>)
+    fun updateChatRoomTitleById(chatroomId: String, title: String)
+    fun updateChatRoomUnreadById(chatroomId: String, unread: Int)
+    fun plusChatRoomUnreadById(chatroomId: String, unread: Int)
+    fun updateChatRoomContentById(chatroomId: String, content: String, updatedAt: Date)
+    fun deleteChatRoom(chatroomId: String)
 }
