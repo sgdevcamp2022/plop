7ccc0860f1a2818c292f91be045ce9223250a9f3
GaheeKim <aqswefr@naver.com>
2023-02-03 14:28:20 +0900

    feat: add chat view model
    [Feat]: add chat view model

--- src/mobile/android/app/src/main/java/com/plop/plopmessenger/presentation/component/TopBar.kt
+++ src/mobile/android/app/src/main/java/com/plop/plopmessenger/presentation/component/TopBar.kt
@@ -164,7 +164,7 @@ fun ChatTopBar(
     onClick: (String) -> Unit,
     upPress: () -> Unit,
     chatroomTitle: String,
-    chatId: String,
+    chatId: String?,
     images: List<String?>,
     btnColor: Color = MaterialTheme.colors.primary,
     modifier: Modifier = Modifier
@@ -175,7 +175,7 @@ fun ChatTopBar(
             .statusBarsPadding()
             .padding(horizontal = TopBarValue.HorizontalPadding)
             .height(TopBarValue.TopBarHeight)
-            .clickable { onClick(chatId) },
+            .clickable { if(!chatId.isNullOrBlank()) onClick(chatId) },
         verticalAlignment = Alignment.CenterVertically
     ) {
         IconButton(
