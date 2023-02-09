6eee6153f26c5d2b629aa7023311f2ef23bdd6c9
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2023-02-09 15:07:59 +0900

    [Backend] Feature/chat exception (#32)
    * build: Add application name
    
    * feat: Add newMessagesAtRoom method
    
    새 메시지 API
    
    * feat: Add Exceptions
    
    예외처리 코드 추가
    
    * build: Add application name

--- src/server/chat/src/main/java/smilegate/plop/chat/exception/ErrorCode.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/exception/ErrorCode.java
@@ -6,7 +6,9 @@ import java.time.LocalDateTime;
 
 @Getter
 public enum ErrorCode {
-    GROUP_MEMBER_SIZE_ERROR(4001,"CHATTING-001","그룹멤버는 3명이상이여야함."),
+    DM_MEMBER_ERROR(4001,"CHATTING-001","상대방id가 있어야함"),
+    GROUP_MEMBER_SIZE_ERROR(4002,"CHATTING-002","그룹멤버는 3명이상이여야함"),
+    ROOM_NOT_FOUND_ERROR(4003,"CHATTING-003","해당 채팅방이 없음"),
     JWT_PARSING_FAILED(4201, "CHATTING-201", "인증에 실패한 경우");
 
     private final int status;
