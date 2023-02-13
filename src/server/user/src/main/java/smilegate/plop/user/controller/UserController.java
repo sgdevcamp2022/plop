b264fdf002822e59fe1c967a0d37ff8095fe7f74
jhl8109 <jhl81094@gmail.com>
2023-02-13 23:59:56 +0900

    feat: Add profile upload function 프로필 사진 업로드 기능 추가

--- src/server/user/src/main/java/smilegate/plop/user/controller/UserController.java
+++ src/server/user/src/main/java/smilegate/plop/user/controller/UserController.java
@@ -1,12 +1,20 @@
 package smilegate.plop.user.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import smilegate.plop.user.domain.user.UserEntity;
+import smilegate.plop.user.dto.request.RequestProfile;
 import smilegate.plop.user.dto.response.ResponseDto;
 import smilegate.plop.user.dto.response.ResponseFriend;
 import smilegate.plop.user.dto.response.ResponseProfile;
@@ -16,8 +24,8 @@ import smilegate.plop.user.security.JwtTokenProvider;
 import smilegate.plop.user.service.FriendService;
 import smilegate.plop.user.service.UserService;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 @RequestMapping("/")
@@ -26,6 +34,8 @@ public class UserController {
     private Environment env;
     private UserService userService;
     private JwtTokenProvider jwtTokenProvider;
+    @Value("${cloud.aws.s3.bucket}")
+    private String S3Bucket;
 
     @Autowired
     public UserController(Environment env, UserService userService, JwtTokenProvider jwtTokenProvider) {
@@ -47,12 +57,10 @@ public class UserController {
     }
     @PutMapping("/profile")
     public ResponseEntity<ResponseDto> changeProfile(
-            @RequestParam String target,
-            @RequestParam String img,
-            @RequestParam String nickname) {
+            RequestProfile profile) {
         // jwt를 통해 본인이 맞는지 확인해야 할듯 로직 추가 필요
 //        String jwt = jwtTokenProvider.removeBearer(bearerToken);
-        ResponseProfile user = userService.putProfile(target, nickname,img);
+        ResponseProfile user = userService.putProfile(profile);
         ResponseDto responseDto;
         if (user != null)
             responseDto = new ResponseDto<>("SUCCESS", "change profile successfully", user);
@@ -60,6 +68,24 @@ public class UserController {
             responseDto = new ResponseDto<>("FAIL", "change profile request failed", user);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
+//    @PutMapping("/upload")
+//    public ResponseEntity<ResponseDto> upload(RequestProfile requestProfile) throws IOException {
+//        MultipartFile multipartFile = requestProfile.getImg();
+//        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+//
+//        ObjectMetadata objMeta = new ObjectMetadata();
+//        objMeta.setContentLength(multipartFile.getInputStream().available());
+//
+//        amazonS3Client.putObject(S3Bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+//        String uri = amazonS3Client.getUrl(S3Bucket, s3FileName).toString();
+//        ResponseProfile responseProfile = new ResponseProfile(requestProfile.getTarget(),requestProfile.getTarget(),
+//                Map.of("nickname",requestProfile.getNickname(),
+//                        "img",uri));
+//        ResponseDto responseDto =  new ResponseDto<>("SUCCESS", "change profile successfully", responseProfile);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+//
+//    }
     @GetMapping("/search")
     public ResponseEntity<ResponseDto> searchUser(
             @RequestParam String target) {
