6e1fa858069f1253f43548e836d42f081edb7846
이제호 <78259314+jhl8109@users.noreply.github.com>
2023-02-14 00:15:07 +0900

    [Backend] Feature/profile image (#49)
    * feat: Add profile upload function
    프로필 사진 업로드 기능 추가

--- src/server/user/src/main/java/smilegate/plop/user/service/UserService.java
+++ src/server/user/src/main/java/smilegate/plop/user/service/UserService.java
@@ -1,21 +1,28 @@
 package smilegate.plop.user.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import smilegate.plop.user.domain.friend.FriendEntity;
 import smilegate.plop.user.domain.friend.FriendRepository;
 import smilegate.plop.user.domain.user.UserEntity;
 import smilegate.plop.user.domain.user.UserRepository;
 import smilegate.plop.user.dto.UserDto;
+import smilegate.plop.user.dto.request.RequestProfile;
 import smilegate.plop.user.dto.response.ResponseProfile;
 import smilegate.plop.user.dto.response.ResponseUser;
 import smilegate.plop.user.model.JwtUser;
 import smilegate.plop.user.security.JwtTokenProvider;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -25,12 +32,18 @@ public class UserService {
 
     JwtTokenProvider jwtTokenProvider;
 
+    AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    String S3Bucket;
+
     @Autowired
     public UserService(UserRepository userRepository, FriendRepository friendRepository,
-               JwtTokenProvider jwtTokenProvider){
+               JwtTokenProvider jwtTokenProvider, AmazonS3Client amazonS3Client){
         this.userRepository = userRepository;
         this.friendRepository =friendRepository;
         this.jwtTokenProvider =jwtTokenProvider;
+        this.amazonS3Client = amazonS3Client;
     }
 
     public ResponseProfile getProfile(String target) {
@@ -45,22 +58,34 @@ public class UserService {
         }
     }
 
-    public ResponseProfile putProfile(String target, String nickname, String img) {
-        UserEntity user = userRepository.findByUserIdOrEmail(target,target);
+    public ResponseProfile putProfile(RequestProfile profile) {
+        UserEntity user = userRepository.findByUserIdOrEmail(profile.getTarget(),profile.getTarget());
 
-        if (user==null) {
-            return null;
-        } else {
-            user.setProfile(Map.of(
-                    "nickname" , nickname,
-                    "img" , img
-            ));
-            UserEntity savedUser = userRepository.save(user);
-            ResponseProfile responseProfile = new ResponseProfile(
-                    savedUser.getUserId(), savedUser.getEmail(), savedUser.getProfile()
-            );
-            return responseProfile;
+        if (user != null) {
+            MultipartFile multipartFile = profile.getImg();
+            String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+            ObjectMetadata objMeta = new ObjectMetadata();
+            try {
+                objMeta.setContentLength(multipartFile.getInputStream().available());
+
+                amazonS3Client.putObject(S3Bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+                String uri = amazonS3Client.getUrl(S3Bucket, s3FileName).toString();
+
+                user.setProfile(Map.of(
+                        "nickname", profile.getNickname(),
+                        "img", uri
+                ));
+                UserEntity savedUser = userRepository.save(user);
+                ResponseProfile responseProfile = new ResponseProfile(
+                        savedUser.getUserId(), savedUser.getEmail(), savedUser.getProfile()
+                );
+                return responseProfile;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return null;
     }
 
     public List<ResponseProfile> searchUser(String target) {
