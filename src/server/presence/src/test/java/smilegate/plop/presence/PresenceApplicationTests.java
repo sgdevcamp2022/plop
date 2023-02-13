69045d50fea3fa7b2f480a65deba53933f0dfd6a
llsrrll96 <llsrrll96@gmail.com>
2023-02-13 14:09:32 +0900

    remove: test case

--- src/server/presence/src/test/java/smilegate/plop/presence/PresenceApplicationTests.java
+++ src/server/presence/src/test/java/smilegate/plop/presence/PresenceApplicationTests.java
@@ -1,21 +1,9 @@
 package smilegate.plop.presence;
 
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import smilegate.plop.presence.dto.PresenceUserDto;
-import smilegate.plop.presence.dto.response.ResponsePresenceUsers;
-import smilegate.plop.presence.service.FriendService;
-import smilegate.plop.presence.service.PresenceService;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class PresenceApplicationTests {
