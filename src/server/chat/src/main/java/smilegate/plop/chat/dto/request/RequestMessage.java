1cb57992b92e51b06d8b2de696d6b22d674aacd1
llsrrll96 <llsrrll96@gmail.com>
2023-02-14 15:14:53 +0900

    feat: add push for chat message

---
+++ src/server/chat/src/main/java/smilegate/plop/chat/dto/request/RequestMessage.java
@@ -0,0 +1,14 @@
+package smilegate.plop.chat.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class RequestMessage {
+    String title;
+    String body;
+    List<String> target;
+}
