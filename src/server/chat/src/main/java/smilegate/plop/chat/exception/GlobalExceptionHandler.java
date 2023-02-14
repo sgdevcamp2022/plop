1052a3f0459406dc840c629c15b65c1dbba63fc5
llsrrll96 <llsrrll96@gmail.com>
2023-02-14 15:17:12 +0900

    feat: add exceptions
    UnexpectedTypeException
    MethodArgumentNotValidException

--- src/server/chat/src/main/java/smilegate/plop/chat/exception/GlobalExceptionHandler.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/exception/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package smilegate.plop.chat.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.validation.UnexpectedTypeException;
 import java.time.LocalDateTime;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,6 +21,7 @@ public class GlobalExceptionHandler {
                 .status(exception.getStatus()).errorCode(exception.getCode())
                 .description(exception.getDescription()).errorMsg(exception.getErrorMsg())
                 .timestamp(LocalDateTime.now()).build();
+        log.error("ErrorCode: {}, roomid: {}","ROOM_NOT_FOUND_ERROR", errorResponseDto.getErrorMsg());
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
@@ -24,5 +29,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleBlogAPIException(JwtTokenIncorrectStructureException exception, WebRequest webRequest){
         ErrorResponseDto errorResponseDto = ErrorCode.JWT_PARSING_FAILED.toErrorResponseDto("JWT 파싱 실패");
         return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnexpectedTypeException.class)
+    public ResponseEntity<ErrorResponseDto> handleUnexpectedTypeException(UnexpectedTypeException exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = ErrorCode.REQUEST_PARAM_INVALID.toErrorResponseDto(exception.getMessage());
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = ErrorCode.REQUEST_PARAM_INVALID.toErrorResponseDto(exception.getMessage());
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 }
