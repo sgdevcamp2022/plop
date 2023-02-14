cda9d4ff66618d95200bfbc6e4fefd3dc058ef6a
llsrrll96 <llsrrll96@gmail.com>
2023-02-14 15:15:06 +0900

    chore: change port number

--- src/server/chat/src/main/java/smilegate/plop/chat/controller/PresenceProxy.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/controller/PresenceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import smilegate.plop.chat.dto.request.RequestUsers;
 import smilegate.plop.chat.dto.response.ResponsePresenceUsers;
 
-@FeignClient(name = "presence-service", url = "localhost:8082")
+@FeignClient(name = "presence-service", url = "localhost:8021")
 public interface PresenceProxy {
     @PostMapping("/presence/v1/offline-users")
     ResponsePresenceUsers offlineUsers(@RequestBody RequestUsers requestUsers);
