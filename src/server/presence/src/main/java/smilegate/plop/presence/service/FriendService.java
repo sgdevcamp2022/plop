b1599952f72e41df3583aa5ed353abd6c17dc08d
llsrrll96 <llsrrll96@gmail.com>
2023-02-15 22:24:12 +0900

    chore: log 추가

--- src/server/presence/src/main/java/smilegate/plop/presence/service/FriendService.java
+++ src/server/presence/src/main/java/smilegate/plop/presence/service/FriendService.java
@@ -2,9 +2,9 @@ package smilegate.plop.presence.service;
 
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import smilegate.plop.presence.controller.UserProxy;
-import smilegate.plop.presence.dto.UserDto;
 import smilegate.plop.presence.dto.response.ResponseDto;
 import smilegate.plop.presence.dto.response.ResponseProfile;
 
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FriendService {
@@ -23,8 +24,10 @@ public class FriendService {
             ResponseDto responseDtos = userProxy.getMyFriends(jwt);
             friends = responseDtos.getData().stream().map(ResponseProfile::getUserId).collect(Collectors.toList());
         }catch (FeignException exception){
+            log.error("getMyFriends, 유저서버와 연결되지 않음");
             return new ArrayList<>();
         }catch (NullPointerException exception){
+            log.info("getMyFriends, 친구정보가 없음");
             return new ArrayList<>();
         }
 
