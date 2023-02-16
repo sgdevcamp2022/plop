52d12606e94ec0d15580bbc3be25be6c3e113a92
llsrrll96 <llsrrll96@gmail.com>
2023-02-16 11:14:34 +0900

    feat: add RoomType attr #54
    채팅방 리스트 조회에서 채팅방 타입 추가

--- src/server/chat/src/main/java/smilegate/plop/chat/domain/room/RoomRepositoryImpl.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/domain/room/RoomRepositoryImpl.java
@@ -40,9 +40,7 @@ public class RoomRepositoryImpl implements RoomMongoTemplateRepository{
         ));
 
         query.fields().exclude("_id");
-        query.fields().include("roomId");
-        query.fields().include("title");
-        query.fields().include("members");
+        query.fields().exclude("managers");
 
         return mongoTemplate.find(query,RoomCollection.class);
     }
