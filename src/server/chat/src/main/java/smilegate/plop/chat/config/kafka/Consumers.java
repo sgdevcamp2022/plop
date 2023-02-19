61fdca2be65260ebafd1343973c91e2c321cd57d
llsrrll96 <llsrrll96@gmail.com>
2023-02-19 17:51:17 +0900

    refactor: Producers, Consumers
    메소드를 각각의 기능에 맞게 변경
    Producers: FIRST 타입일때 방생성 메시지 send
    Consumers: 조건문을 Producers 객체로 이동

--- src/server/chat/src/main/java/smilegate/plop/chat/config/kafka/Consumers.java
+++ src/server/chat/src/main/java/smilegate/plop/chat/config/kafka/Consumers.java
@@ -6,38 +6,20 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import smilegate.plop.chat.dto.ChatMessageDto;
-import smilegate.plop.chat.dto.MessageType;
 import smilegate.plop.chat.dto.RoomMessageDto;
 import smilegate.plop.chat.dto.response.RespRoomDto;
-import smilegate.plop.chat.service.ChatRoomService;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class Consumers {
     private final SimpMessagingTemplate template;
-    private final ChatRoomService chatRoomService;
 
     @KafkaListener(groupId = "${spring.kafka.chat-consumer.group-id}" ,topics="${kafka.topic.chat-name}")
     public void listenChat(ChatMessageDto chatMessageDto){
-        if(chatMessageDto.getMessage_type()==MessageType.FIRST){
-            RespRoomDto respRoomDto = chatRoomService.getChatRoomInfo(chatMessageDto.getRoom_id()); // 채팅방 무조건 있다고 신뢰
-            /**
-             * 상대방 userid 찾기
-             *  if 인덱스 0 이 보낸사람id랑 다르면 0이 상대방 userid, 보낸사람이랑 같으면 1이 상대방 userid
-             */
-            String userid = !respRoomDto.getMembers().get(0).getUserId().equals(chatMessageDto.getSender_id()) ?
-                    respRoomDto.getMembers().get(0).getUserId() : respRoomDto.getMembers().get(1).getUserId();
-            template.convertAndSend("/chatting/topic/new-room/"+userid,respRoomDto);
-        }
-        log.info("채팅방: {}, 보낸사람: {}, 메시지: {}", chatMessageDto.getRoom_id(), chatMessageDto.getSender_id(), chatMessageDto.getContent());
         template.convertAndSend("/chatting/topic/room/"+chatMessageDto.getRoom_id(), chatMessageDto);
     }
 
-    /**
-     * 자신을 제외한 룸 멤버들에게 방생성 정보 전달
-     * @param roomMessageDto
-     */
     @KafkaListener(groupId = "${spring.kafka.room-consumer.group-id}",topics = "${kafka.topic.room-name}", containerFactory = "kafkaListenerContainerFactory")
     public void listenGroupCreation(RoomMessageDto roomMessageDto){
         RespRoomDto respRoomDto = roomMessageDto.getRespRoomDto();
