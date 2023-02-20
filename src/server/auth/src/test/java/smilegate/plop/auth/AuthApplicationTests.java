6cd5a07723fa449aa65a96b154b8864dbfdeb1ba
이제호 <78259314+jhl8109@users.noreply.github.com>
2023-02-20 21:51:42 +0900

    [Backend[ Feature/be-auth-test (#69)
    test: check DB enum and time auditing

--- src/server/auth/src/test/java/smilegate/plop/auth/AuthApplicationTests.java
+++ src/server/auth/src/test/java/smilegate/plop/auth/AuthApplicationTests.java
@@ -1,7 +1,13 @@
 package smilegate.plop.auth;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import smilegate.plop.auth.domain.UserEntity;
+import smilegate.plop.auth.domain.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.Map;
 
 @SpringBootTest
 class AuthApplicationTests {
